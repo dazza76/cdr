e29c9d833633d32b0e1d4cf2d8b256a8a467bf33
Tyurin D <fobia3d@gmail.com>
2013-07-27 23:38:33 +0400

    test

---
+++ test/invalid_events/filter.php
@@ -0,0 +1,35 @@
+﻿<?php
+	function form_oper_list($name)
+	{
+		global $oper_list, $db;
+		$tmp = $db->query("SELECT name,agentid FROM queue_agents") or die(mysql_error());
+		while($tmprow = $tmp->fetch_array())
+		{
+			$result .= "<input type=\"checkbox\" name=\"".$name."_".$tmprow['agentid']."\"";
+			if($_GET[$name.'_'.$tmprow['agentid']] == 'on')
+				$result .= "checked";
+			$result .= ">".$tmprow['name']."<br/>";
+		}
+		return $result;
+	};
+
+	foreach($_GET as $key => $value)
+		if(preg_match("/^oper_/",$key))
+			$oper_list .= ",'".str_replace('oper_','',$key)."'";
+	$oper_list = substr($oper_list,1);
+	if(($_GET['datefrom']) && (preg_match("/^2[0123456789]{3}-[01][0123456789]-[0123][0123456789] [012][0123456789]\:[012345][0123456789]$/",$_GET['datefrom'])))
+		$filter .= " dateofevent >= '".$_GET['datefrom']."' AND";
+	if(($_GET['dateto']) && (preg_match("/^2[0123456789]{3}-[01][0123456789]-[0123][0123456789] [012][0123456789]\:[012345][0123456789]$/",$_GET['dateto'])))
+		$filter .= " dateofevent <= '".$_GET['dateto']."' AND";
+	if($oper_list)
+		$filter .= " agentid IN (".$oper_list.") AND";
+
+	$html_out = "<FORM action=\"index.php\">";
+	$html_out .= "C <INPUT name=\"datefrom\" placeholder=\"ГГГГ-ММ-ДД ЧЧ:ММ\" maxlength=\"16\"\" size=\"16\" value=\"".$_GET['datefrom']."\"> ";
+	$html_out .= "по <INPUT name=\"dateto\" placeholder=\"ГГГГ-ММ-ДД ЧЧ:ММ\" maxlength=\"16\"\" size=\"16\" value=\"".$_GET['dateto']."\"> ";
+	$html_out .= "<br>Операторы:<br/>".form_oper_list('oper');
+	$html_out .= "<INPUT type=\"submit\">";
+	$html_out .= "</FORM>";
+echo $oper_list;
+	echo $html_out;
+?>
---
+++ test/invalid_events/header.php
@@ -0,0 +1,51 @@
+﻿<?php
+	function showoper($id)
+	{
+		// mysql_query("SET NAMES UTF8");
+		global $db;
+		// $res = mysql_query("SELECT name FROM queue_agents WHERE agentid = $id;");
+		$res = $db->query("SELECT name FROM queue_agents WHERE agentid = $id;");
+
+		if($res->num_rows)
+			$row = $res->fetch_array();
+		else
+			return 'Неизвестно';
+		return $row[0];
+	}
+	function form_switch($name,$value)
+	{
+		$tmparray = array("Вкл." => 'yes',"Выкл." => 'no');
+		foreach($tmparray as $tmpkey => $tmpvalue)
+		{
+			$result .= "<INPUT type=\"radio\" name=\"".$name."\" value=\"".$tmpvalue."\" ";
+			if($tmpvalue == $value)
+				$result .= "checked ";
+			$result .= "/>".$tmpkey."<br/>";
+		}
+
+		return $result;
+	}
+	function show_switch($value)
+	{
+		$tmparray = array('yes' => "Вкл.",'no' => "Выкл.");
+		return $tmparray[$value];
+	}
+
+
+
+
+	$dbsrv = "127.0.0.1";
+	$dbusr = "root";
+	$dbpwd = "";
+	// $dbcon = mysql_connect($dbsrv,$dbusr,$dbpwd);
+	$db = new mysqli($dbsrv,$dbusr,$dbpwd, "asterix");
+	// mysql_query("SET NAMES UTF8");
+	$db->set_charset("UTF8");
+	// mysql_select_db("asterix");
+	// $goto = array('index.php' => "Главная",'settings.php' => "Настройки");
+	// foreach($goto as $key => $value)
+//	if($_SERVER['PHP_SELF'] == '/invalid_events/'.$key)
+	//		echo('['.$value.']&nbsp;');
+		//else
+			// echo('<a href="'.$key.'">['.$value.']</a>&nbsp;');
+?>
