6d87a1e0cb568bcb1c3f2fc3c6f31ab44e3c8867
dazza76 <darren_philips@hotmail.com>
2013-08-12 13:29:42 +1000

    translate

--- login.php
+++ login.php
@@ -89,7 +89,7 @@ $app = new Application();
 
     </head>
     <body>
-        <div id="login_box" class="dialog hidden" title="Авторизация" >
+        <div id="login_box" class="dialog hidden" title="Login" >
             <form id="loginform" method="post" >
 
                 <input name="login" type="hidden" value="1" />
@@ -97,19 +97,19 @@ $app = new Application();
 <!--                 <div class="ui-widget hidden">
                     <div class="ui-state-error ui-corner-all" style="margin-top: 10px; padding: 0 .7em;">
                         <p><span class="ui-icon ui-icon-alert" style="float: left; margin-right: .3em;"></span>
-                            <strong>Ошибка:</strong> Не верно введен логин или пароль.</p>
+                            <strong>Error:</strong> Invalid Password.</p>
                     </div>
                 </div> -->
 
                 <div style="padding: 10px 0px">
                     <div class="clear clear_fix bigblock">
-                        <div class="label fl_l ta_r">Логин:</div>
+                        <div class="label fl_l ta_r">UserName:</div>
                         <div class="labeled fl_l">
                             <input name="slogin" type="text" class="ui-widget-content ui-corner-all" />
                         </div>
                     </div>
                     <div class="clear clear_fix bigblock">
-                        <div class="label fl_l ta_r">Пароль:</div>
+                        <div class="label fl_l ta_r">Password:</div>
                         <div class="labeled fl_l">
                             <input name="spass" type="password" class="ui-widget-content ui-corner-all" />
                         </div>
