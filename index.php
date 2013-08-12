c39127c0af49f36b985515455c337aed1bb068ec
dazza76 <darren_philips@hotmail.com>
2013-08-12 13:35:54 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-/ / TODO Task 1 [Done]: Thursday, December 06, 2012 5:20 PM
-/ * ------------------------------------------------ ----------------------------
+// TODO Task 1 [Done]: Thursday, December 06, 2012 5:20 PM
+/* ------------------------------------------------ ----------------------------
  * I send you a new batch =)
  * There is a database dump (as one plate is old, not yet implemented the new live)
  * Example of our version, and description fields.
@@ -14,23 +14,23 @@
  * + Graphics from the same
  * Page with the addition of new operators (queue_agents)
  * + Authentication sessions on two levels - the user (only reports) and the administrator.
- -------------------------------------------------- -------------------------- * /
+ -------------------------------------------------- -------------------------- */
 
-/ / TODO Task 2: Sippeers.
-/ *
+// TODO Task 2: Sippeers.
+/*
  * This form is to add. Was still a need for editing and output the list to do.
  * Accordingly, it all goes to the settings, together with operators.
  * Not really, sippeers to queues is irrelevant. In her account settings
  * Records for SIP-clients, ie phones, softphones etc.
- * /
-/ *
+ */
+/*
  * Docs: / sippeers
  * I enclose two dump both the workers in the fields table (main - main settings, expert - advanced, you will need to hide them under spoiler, hidden - deduce fields are updated asterisk).
  * The same script to add, a simple shaper.
- * /
+ */
 
-/ / TODO Task 3 [Done]: Friday, February 22, 2013 3:56 AM
-/ * ------------------------------------------------ ----------------------------
+// TODO Task 3 [Done]: Friday, February 22, 2013 3:56 AM
+/* ------------------------------------------------ ----------------------------
  * + 1) Need checkbox "Only mobile" in razgvoorov records and statistics. On it
  * Choose only the incoming type "9HHHHHHHHH" and outgoing type "[9] 89XXXXXXXXX".
  + * 2) In addition the operators do not need the "phone that runs" with
@@ -40,8 +40,8 @@
  * Infancy, we are developing an algorithm)
  -------------------------------------------------- -------------------------- * /
 
-/ / TODO Task 4: Office bursts. docs/task5 Friday, February 22, 2013 3:56 AM
-/ *
+// TODO Task 4: Office bursts. docs/task5 Friday, February 22, 2013 3:56 AM
+/*
  * Dump the table to dump the report to manage queues whore.
  * On the second plate is more or less simple:
  * Name of the queue interface (in brackets - device, we will have the form
@@ -50,10 +50,10 @@
  * Operator, uniqueid - not used, but it would asterisk, there can be
  * Md5 wedge in, the main thing that was unique. Paused - 0 or 1,
  * Depending on whether the received calls to the agent.
- * /
+ */
 
-/ / TODO Task 5: The supervisor. docs/task5
-/ *
+// TODO Task 5: The supervisor. docs/task5
+/*
  * + 1) Recorded Tab1 and Tab2 call "Call" and "Answering machine"
  + * 2) In the recording, too, need the output name of the operator.
  * + 3) In the setting of operators to finish editing and deleting.
@@ -71,10 +71,10 @@
  * The level of service - the percentage of calls served within the specified time
  * (Specified in the form itself). During the service there is
  * Waiting and talking.
- * /
+ */
 
-/ / TODO Task 6: Supervisor - Improvements
-/ *
+// TODO Task 6: Supervisor - Improvements
+/*
  * 1) The dynamic update supervisors.
  * 2) Time counters must tick, after all.
  * 3) The formula Service Level: Service Level,% = Answered Less X seconds / Entered
@@ -90,20 +90,20 @@
  * 6) In the "Operators" only show registered and serving
  * Queue specified in the config file.
  * 7) In the settings do another plug - "Pause".
- * /
+ */
 
 
-/ *
+/*
 logag look at what filters it receives and from where
 session parametr 1 - indicates that the filters are taken from the session
 [1:34:58] Dmitry: sabmite steranes all previous filters, and the currently selected setting new
 [1:35:23] Dmitry: all filters sbivayutsya with any het request, except the section
 [1:35:56] Dmitry: it is not the reason for yavlyaetsya zbrosa of session
 
-* /
+*/
 
 
-/ *
+/*
 1. [+] The date format tables EVERYWHERE cause DD.MM.YYYY HH: MM: SS
 2. [+ -] Tick "Mobile" in a report by one
       > Besides tables. Table does not respond to the filters «VIP» and "mobile"
@@ -133,10 +133,10 @@ session parametr 1 - indicates that the filters are taken from the session
 22. [-] Operators to report Supervisor operators select only those whose state <> 'out'
 23. [+ -] Button "Export to Excel»
       > Until ready to "Profile Call", "Supervisor: Distribution", "Monthly Report"
- * /
-      / / 1. We had no time will be displayed columns for "processing" or "call."
+ */
+// 1. We had no time will be displayed columns for "processing" or "call."
 
-/ *
+/*
 Thus, in the system:
 1) Record.
 There is no record of conversations answerphone. They just do not appear.
@@ -197,9 +197,9 @@ Send full Bild
 [0:30:30] Vadim Tesalov: When some mobile clients empty schedule
 [0:33:06] Vadim Tesalov: In avtoifnormatore ignore settings result /
 [0:34:19] Vadim Tesalov: by pressing the "n attempts to" let the spoiler reveals the details
-* /
+*/
 
-/ *
+/*
 1) + + Let's repeat the cap periodically in the table (as in phpmyadmin, the findings for large cap repeated periodically as a row in the table)
 2) - Autoinformlog.php not plow, maybe I tuplyu where to look?
 3) + + Sliders can still replace windows?
@@ -209,32 +209,31 @@ Send full Bild
 6) - Tick, remember, huh?)
 7) + + In operlog want to have a filter on the actions ("challenges", "action")
 8) + + Reports operators do not choose NONE. NONE of the request to remove the «memberId IN ('..', '..', 'NONE', '..', '..')»
-* /
+*/
 
-/ / ------------------------------------------------ ---------------------------
-/ / TODO Task 7: Current
-/ / TODO Task 7.1: Time
-/ / SELECT MIN (NOW ()-datetime) FROM ((SELECT datetime, action FROM `agent_log` WHERE agentid = 1024 AND action IN ('Login', 'unpause', 'unaftercal') ORDER BY `agent_log`. `Datetime `DESC LIMIT 1) UNION ALL (SELECT timestamp as datetime, status AS action FROM call_status WHERE memberId = 1024 AND status LIKE 'COMPLETE%' ORDER BY timestamp DESC LIMIT 1)) AS temp;
-/ / + Check indexing records (at the same time and compare avtoifnormator, and call recording)
-/ / + Enable auto-update and sort where possible.
-/ / TODO Task Schedule 7.3 (Instead of making bets fact, we consider the total duration as a monthly statement) + export to xls.
-/ / TODO Task 7.4've stole a report on the working time of the lane + selection of lines by clicking a different color.
-/ / TODO Task 7.5 settings should queues.
-/ / Setup + answerphone
-/ / TODO Task 7.7 Different breaks (user)
+// ------------------------------------------------ ---------------------------
+// TODO Task 7: Current
+// TODO Task 7.1: Time
+// SELECT MIN (NOW ()-datetime) FROM ((SELECT datetime, action FROM `agent_log` WHERE agentid = 1024 AND action IN ('Login', 'unpause', 'unaftercal') ORDER BY `agent_log`. `Datetime `DESC LIMIT 1) UNION ALL (SELECT timestamp as datetime, status AS action FROM call_status WHERE memberId = 1024 AND status LIKE 'COMPLETE%' ORDER BY timestamp DESC LIMIT 1)) AS temp;
+// + Check indexing records (at the same time and compare avtoifnormator, and call recording)
+// + Enable auto-update and sort where possible.
+// TODO Task Schedule 7.3 (Instead of making bets fact, we consider the total duration as a monthly statement) + export to xls.
+// TODO Task 7.4've stole a report on the working time of the lane + selection of lines by clicking a different color.
+// TODO Task 7.5 settings should queues.
+// Setup + answerphone
+// TODO Task 7.7 Different breaks (user)
 
 
-
-/ / ------------------------------------------------ ---------------------------
-/ / Wed while lifting the handset, s
-/ /
-/ / + + Status must finish the report with the plus sign (instead aftercall and unaftercall), time in HH: MM: SS (in duration of interruptions)
-/ / Time ticking away in the supervisor.
-/ / + + If you choose to record conversations in 100 or 500 records per page, the pages do not switch
-/ / + + The report Operators Workload check-cap (odinkaovyh two columns with different data)
-/ / Log (here you are free to do whatever you want, but it is necessary to restrict access)!
-/ /
-/ / Print_r ($ _SERVER);
+// ------------------------------------------------ ---------------------------
+// Wed while lifting the handset, s
+//
+// + + Status must finish the report with the plus sign (instead aftercall and unaftercall), time in HH: MM: SS (in duration of interruptions)
+// Time ticking away in the supervisor.
+// + + If you choose to record conversations in 100 or 500 records per page, the pages do not switch
+// + + The report Operators Workload check-cap (odinkaovyh two columns with different data)
+// Log (here you are free to do whatever you want, but it is necessary to restrict access)!
+//
+// Print_r ($ _SERVER);
 
  require_once 'protected/bootstrap.php';
 
